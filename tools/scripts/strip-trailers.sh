d25d9f719d7182f809f725d99df796e2f8c7e082
theprabir <thejnvprabir@gmail.com>
2026-09-23 14:17:22 +0530

    Remove one-shot trailer cleanup script

--- tools/scripts/strip-trailers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Strips bot attribution trailers from commit messages.
-# Used by: git filter-branch --msg-filter "sh <absolute-path-to-this-file>" -- --all
-sed -e '/Generated with Codebuff/d' \
-    -e '/Co-Authored-By: Codebuff/d'
